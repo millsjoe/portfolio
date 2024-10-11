dd1f5fc9b9b211653db81b218bdc648f54f133d5
Joe Mills <joe.mills@skybettingandgaming.com>
2024-10-11 12:11:27 +0100

    cat is gone

---
+++ src/app/components/header.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import React from "react";
+import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
+
+interface SocialMediaIconProps {
+  platform: string;
+  url: string;
+  icon: JSX.Element;
+}
+
+const socialMediaLinks: SocialMediaIconProps[] = [
+  {
+    platform: "LinkedIn",
+    url: "https://www.linkedin.com/in/joe-mills/",
+    icon: <FaLinkedin />,
+  },
+  {
+    platform: "Twitter",
+    url: "https://x.com/iamjoemills",
+    icon: <FaTwitter />,
+  },
+  {
+    platform: "Github",
+    url: "https://github.com/millsjoe",
+    icon: <FaGithub />,
+  },
+];
+
+export const SocialMedia = () => {
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center">
+      <div className="flex space-x-4">
+        {socialMediaLinks.map(({ platform, url, icon }) => (
+          <a
+            key={platform}
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-500 hover:text-blue-500"
+          >
+            {icon}
+          </a>
+        ))}
+      </div>
+      <Image
+        src="https://avatars.githubusercontent.com/u/42638335?v=4"
+        alt="Joe Mills"
+        width={50}
+        height={50}
+        className="rounded-full"
+      />
+    </div>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,17 @@
-import Image from "next/image";
 import { AboutMe } from "./components/about";
 import { Experience } from "./components/experience";
+import { SocialMedia } from "./components/header";
+import React from "react";
 
 export default function Home() {
   return (
-    <div className="bg-gray-100 flex items-center justify-center min-h-screen">
-      <div className="flex flex-col md:flex-row gap-4">
-        {/* <AboutMe />
-        <Experience /> */}
-
-        <Image
-          src="/cat-jam.gif"
-          alt="Cat"
-          width={300}
-          height={300}
-          className="animate-bounce"
-        />
+    <div className="flex flex-col md:flex-col gap-2 items-center justify-center">
+      <SocialMedia />
+      <div className="bg-gray-100 flex items-center justify-center min-h-screen">
+        <div className="flex flex-col md:flex-row gap-4">
+          <AboutMe />
+          <Experience />
+        </div>
       </div>
     </div>
   );
